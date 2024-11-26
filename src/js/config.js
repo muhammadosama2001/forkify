cf25aeecedefaa3b84b3be1899626f2409475aab
Osama <mosamafaisal01@gmail.com>
2024-11-26 22:22:45 +0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a709c757-92f9-4120-a3f5-b6a970620bd3';
+export const MODAL_CLOSE_SEC = 2.5;
